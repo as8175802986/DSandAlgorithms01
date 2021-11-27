7987864f2613e98ee7a81a08bec5c64e80729b8a
“Arunesh <as8175802986@gmail.com>
2021-11-27 08:37:30 +0530

    [Arunesh] Added: code for Ordered List

---
+++ DSandAlgoProg/OrderedList.cs
@@ -0,0 +1,41 @@
+﻿using DSandAlgoProg.Utility;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSandAlgoProg
+{
+    class OrderedList
+    {
+        public static void Ordered(int searchNum)
+        {
+            string text = File.ReadAllText(@"C:\Users\DELL\source\Repos\DSandAlgoProg\Utility\NumberList.txt");
+            string[] textArry = text.Split();
+            CustomLinkedList<int> list = new CustomLinkedList<int>();
+
+            int[] arr = new int[textArry.Length];
+            for (int i = 0; i < textArry.Length; i++)
+            {
+                int a = Convert.ToInt32(textArry[i]);
+                arr[i] = a;
+            }
+            Array.Sort(arr);
+            list.Display();
+
+            if (list.Search(searchNum))
+            {
+                list.DeleteElement(searchNum);
+            }
+            else
+            {
+                list.Sort(searchNum);
+            }
+            string textFile = list.ReturnString();
+            File.ReadAllText(@"C:\Users\DELL\source\Repos\DSandAlgoProg\Utility\NumberList.txt");
+            list.Display();
+        }
+    }
+}
