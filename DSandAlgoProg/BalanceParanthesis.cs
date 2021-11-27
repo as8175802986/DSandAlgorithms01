4a141bbfe0ab53dd3a9144da9ce118a7cb0caf2b
“Arunesh <as8175802986@gmail.com>
2021-11-27 10:04:52 +0530

    [Arunesh] Added: code for Balance Parenthesis

---
+++ DSandAlgoProg/BalanceParanthesis.cs
@@ -0,0 +1,39 @@
+﻿using DSandAlgoProg.Utility;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSandAlgoProg
+{
+    class BalanceParanthesis
+    {
+        public static void Balance()
+        {
+            string text = File.ReadAllText(@"E:\DS-Algo01\DSandAlgorithms01\DSandAlgoProg\Utility\Airthmatic.txt");
+            string[] textArry = text.Split();
+            CustomStack<string> cs = new CustomStack<string>();
+            foreach (var result in textArry)
+            {
+                if (result == "(")
+                {
+                    cs.Push("(");
+                }
+                else
+                {
+                    cs.Pop(")");
+                }
+                if (cs.Isempty())
+                {
+                    Console.WriteLine("Balaenced Parathensis");
+                }
+                else
+                {
+                    Console.WriteLine("Not a Balanced Parathensis");
+                }
+            }
+        }
+    }
+}
